84c5fce3e8df8360201cc6432901b472c5c93e04
3LucasZ <lucas.zheng@warriorlife.net>
2024-07-29 10:18:00 -0700

    Official Icon

--- src/app/api/adb/connect/route.ts
+++ src/app/api/adb/connect/route.ts
@@ -1,18 +1,9 @@
-import { AdbDaemonDirectSocketsDevice } from "@/app/services/adb_helper";
-import {
-  Adb,
-  AdbPacketData,
-  AdbPacketInit,
-  AdbServerClient,
-} from "@yume-chan/adb";
-import { AdbServerNodeTcpConnector } from "@yume-chan/adb-server-node-tcp";
-import { Consumable } from "@yume-chan/stream-extra";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const util = require("util");
   const exec = util.promisify(require("child_process").exec);
-  const { stdout, stderr } = await exec("adb devices");
+  const { stdout, stderr } = await exec("badcommand");
   console.log("stdout:", stdout);
   console.log("stderr:", stderr);
 
